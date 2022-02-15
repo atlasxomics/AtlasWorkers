b988dee2561a7688672e88705aa9f8e54afb6439
Ubuntu <ubuntu@ip-172-31-101-176.ec2.internal>
2022-02-15 00:35:27 +0000

    ENH: illumina downloading

--- workers/core.py
+++ workers/core.py
@@ -10,6 +10,7 @@ import threading
 import utils
 import hashlib
 import requests
+import math as m
 
 app=Celery('core_task',broker='amqp://'+os.environ['RABBITMQ_HOST'],backend='redis://'+os.environ['REDIS_HOST'])
 
@@ -24,18 +25,37 @@ def task_list(self, *args, **kwargs):
     return taskobject
 
 @app.task(bind=True)
-def list_basespace_datasets(self): ## paging needs to be implemented (default is limit 10)
+def list_basespace_datasets(self,offset=0,dataset_types=['common.fastq'],output_root='data'): ## paging needs to be implemented (default is limit 10)
     self.update_state(state="STARTED")
     config=utils.load_configuration()
-    # aws_s3=utils.AWS_S3(config)
-    # tempDir = Path(config['TEMP_DIRECTORY'])
+    aws_s3=utils.AWS_S3(config)
+    tempDir = Path(config['TEMP_DIRECTORY'])
     base_url = config['BASESPACE_BASE_URL']
     access_token = config['BASESPACE_ACCESS_TOKEN']
     uri = base_url + "/datasets"
     headers={ 'x-access-token': access_token }
-    res = requests.get(uri,headers=headers)
+    params = { 'offset': offset, 'datasettypes': dataset_types }
+    res = requests.get(uri,headers=headers,params=params)
     if res.status_code == 200:
-        return json.loads(res.content)
+        res = json.loads(res.content)
+        if 'Items' in res:
+            for i, doc in enumerate(res['Items']):
+                runid=doc['Name'].split('_')[0]
+                hreffiles = doc['HrefFiles']
+                filehrefs = json.loads(requests.get(hreffiles,headers=headers).content)['Items']
+                res['Items'][i]['file_downloads']=[]
+                for fidx, finfo in enumerate(filehrefs):
+                    outname = "read{}.fq.gz".format(fidx+1)
+                    outkey = "{}/{}/sequences/{}".format(output_root,runid,outname)
+                    res['Items'][i]['file_downloads'].append({
+                        'run_id' : runid,
+                        'name' : finfo['Name'],
+                        'size' : finfo['Size'],
+                        'outpath' : outkey,
+                        'href': finfo['HrefContent'],
+                        'output_exists' : aws_s3.checkFileExist(outkey)
+                    })
+        return res
     else:
         raise Exception("Call to basespace failed")
 
@@ -49,6 +69,33 @@ def call_basespace_href(self,href): ## paging required.
         return json.loads(res.content)
     else:
         raise Exception("Call to basespace failed")
+
+@app.task(bind=True)
+def download_from_basespace_href(self,download_payloads,n_chunk=1024):
+    config=utils.load_configuration()
+    aws_s3=utils.AWS_S3(config)
+    tempDir = Path(config['TEMP_DIRECTORY'])
+    access_token = config['BASESPACE_ACCESS_TOKEN']
+    headers={ 'x-access-token': access_token }
+    print(download_payloads)
+    for idx, dobj in enumerate(download_payloads):
+        r= requests.get(dobj['href'], headers=headers, stream=True)
+        file_size = int(r.headers.get('Content-Length',None))
+        print("Downloading {} from {}".format(dobj['name'], dobj['href']))
+        print("File size : {}".format(file_size))
+        block_size=1024
+        num_bars=m.ceil(file_size / (n_chunk*block_size))
+        tempfilename = tempDir.joinpath("{}.fq.gz".format(utils.get_uuid()))
+        mb = 1024*1024
+        with open(tempfilename,'wb') as f:
+            for i, chunk in enumerate(r.iter_content(chunk_size=n_chunk*block_size)):
+                f.write(chunk)
+                pos = (i+1)*block_size*n_chunk
+                pct = int(pos/file_size*100/len(download_payloads) + (idx)/len(download_payloads)*100)
+                self.update_state(state="PROGRESS", meta={"position": "{:.0f}/{:.0f}MB, File {}/{}".format(pos/mb, file_size/mb,idx+1,len(download_payloads)) , "progress" : pct})
+        aws_s3.uploadFile(tempfilename.__str__(), str(dobj['outpath']))
+        tempfilename.unlink()
+    return None
 
 @app.task(bind=True)
 def list_novogene_batch(self, host, port, username, password , source_root='raw_data', output_root="data", **kwargs):
