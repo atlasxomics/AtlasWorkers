303bd0d21de0fcbb8dbb823c7fa7a3a366ef2bba
Joshua Barnett <joshuab@atlasxomics.com>
2022-03-01 14:11:50 -0500

    Generate h5ad atlasbrowser

--- workers/atlasbrowser.py
+++ workers/atlasbrowser.py
@@ -1,12 +1,21 @@
 import os
+from threading import local
 import traceback
 import yaml,json,csv
+import PIL
 from PIL import Image
 from pathlib import Path
 import time
 import re
 import shutil
-
+from anndata import AnnData
+import matplotlib.pyplot as plt
+import scanpy as sc
+import pandas as pd
+from matplotlib.image import imread
+import json
+from pathlib import Path
+Image.MAX_IMAGE_PIXELS = None
 from celery import Celery
 from celery.signals import worker_init, worker_process_init
 
@@ -29,7 +38,7 @@ def task_list(self, *args, **kwargs):
 @app.task(bind=True)
 def generate_spatial(self, qcparams, **kwargs):
     self.update_state(state="STARTED")
-    self.update_state(state="PROGRESS", meta={"Prepartion": "one" , "progress" : 0})
+    self.update_state(state="PROGRESS", meta={"position": "preparation" , "progress" : 0})
     config=utils.load_configuration()
     aws_s3=utils.AWS_S3(config)
     ## config
@@ -53,16 +62,14 @@ def generate_spatial(self, qcparams, **kwargs):
     allFiles = [i for i in oldFiles if '.json' not in i and 'spatial' not in i]
     
     ### output directories (S3)
-    spatial_dir = Path(root_dir).joinpath(run_id, 'images', 'spatial')
-    figure_dir = Path(root_dir).joinpath(run_id, 'images', 'spatial', 'figure')
-    raw_dir = Path(root_dir).joinpath(run_id,'images','spatial')
+    spatial_dir = Path(root_dir).joinpath(run_id,'images','spatial')
+    figure_dir = Path(root_dir).joinpath(run_id,'images','spatial', 'figure')
+    raw_dir = Path(root_dir).joinpath(run_id,'out','Gene','raw')
     metadata_filename = spatial_dir.joinpath('metadata.json')
     scalefactors_filename = spatial_dir.joinpath('scalefactors_json.json')
     tissue_hires_image_filename = spatial_dir.joinpath('tissue_hires_image.png')
     tissue_lowres_image_filename = spatial_dir.joinpath('tissue_lowres_image.png')
     tissue_positions_filename = spatial_dir.joinpath('tissue_positions_list.csv')
-    tissue_position_list_umi_filename = spatial_dir.joinpath('tissue_positions_list_log_UMI_Genes.csv')
-    barcode_filename = raw_dir.joinpath('barcodes.tsv')
     ### local temp directories
     local_spatial_dir = Path(temp_dir).joinpath(spatial_dir)
     local_spatial_dir.mkdir(parents=True, exist_ok=True)
@@ -77,38 +84,46 @@ def generate_spatial(self, qcparams, **kwargs):
         local_barcodes_filename = 'data/atlasbrowser/barcodes_100.txt'
         row_count = 100
 
-    self.update_state(state="PROGRESS", meta={"running": "two" , "progress" : 20})
+    self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 20})
     barcodes = None
     with open(local_barcodes_filename,'r') as f:
         barcodes = f.read().splitlines()
-    upload_list.append([local_barcodes_filename, barcode_filename])
     ### save metadata & scalefactors
     local_metadata_filename = local_spatial_dir.joinpath('metadata.json')
     local_scalefactors_filename = local_spatial_dir.joinpath('scalefactors_json.json')
     json.dump(metadata, open(local_metadata_filename,'w'), indent=4,sort_keys=True)
     upload_list.append([local_metadata_filename,metadata_filename])
     upload_list.append([local_scalefactors_filename,scalefactors_filename])
-    self.update_state(state="PROGRESS", meta={"position": "two" , "progress" : 40})
+    self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 40})
     ### load image from s3 
     for i in allFiles:
         if 'postb' in i.lower():
             local_image_path = aws_s3.getFileObject(str(i))
-            source_image = cv2.imread(str(local_image_path), cv2.IMREAD_UNCHANGED)
+            source_image = Image.open(str(local_image_path))
+            source_original = Image.open(str(local_image_path))
+            save = source_original.save(str(local_image_path))
             temp = re.compile("(.+\/)(.+)")
             res = temp.search(i).groups() 
             fileName = res[1]
             ### image flipping
             if hflip:
-                source_image=cv2.flip(source_image, 1)
+                horiz = source_image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+                source_image = horiz
             if vflip:
-                source_image=cv2.flip(source_image, 0)
+                vert = source_image.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+                source_image = vert
             if rotation != 0 :
-                for x in range(int(rotation/90)):
-                    source_image=cv2.rotate(source_image, cv2.ROTATE_90_CLOCKWISE)
-            cv2.imwrite(str(local_image_path), source_image)
-            pillow_source_image = Image.open(str(local_image_path))
+                if rotation == 90:
+                    rotation = 270
+                    rotate = source_image.rotate(rotation, expand=True)
+                    source_image = rotate
+                if rotation == 270:
+                    rotation = 90
+                    rotate = source_image.rotate(rotation, expand=True)
+                    source_image = rotate
+            pillow_source_image = source_image
             ### generate cropped images using crop parameters
-            cropped_image = pillow_source_image.crop((crop_coordinates[0]["x"], crop_coordinates[0]["y"], crop_coordinates[1]["x"], crop_coordinates[1]["y"]))
+            cropped_image = pillow_source_image.crop((crop_coordinates[0], crop_coordinates[1], crop_coordinates[2], crop_coordinates[3]))
                 ## high resolution
             tempName = local_figure_dir.joinpath(fileName)
             s3Name = figure_dir.joinpath(fileName)
@@ -175,3 +190,124 @@ def generate_spatial(self, qcparams, **kwargs):
     out=[list(map(str, x)) for x in upload_list]
     #print(len(out))
     return out
+
+
+@app.task(bind=True)
+def generate_h5ad(self, qcparams, **kwargs):
+    self.update_state(state="STARTED")
+    self.update_state(state="PROGRESS", meta={"position": "preparation" , "progress" : 0})
+    config=utils.load_configuration()
+    aws_s3=utils.AWS_S3(config)
+    #config
+    temp_dir = config['TEMP_DIRECTORY']
+    upload_list = []
+    #parameters
+    root_dir = qcparams['root_dir']
+    run_id = qcparams['run_id']
+
+    bar = Path(root_dir + '/'+ run_id + '/out/Gene/raw/barcodes.tsv')
+    pathBar = aws_s3.getFileObject(str(bar))
+    gene = Path(root_dir + '/'+ run_id + '/out/Gene/raw/features.tsv')
+    pathGene = aws_s3.getFileObject(str(gene))
+    matrix = Path(root_dir + '/'+ run_id + '/out/Gene/raw/matrix.mtx')
+    pathMatrix = aws_s3.getFileObject(str(matrix))
+
+    spatial_dir = Path(root_dir).joinpath(run_id,'images','spatial')
+    raw_dir = Path(root_dir).joinpath(run_id,'out','Gene','raw')
+    h5_dir = Path(root_dir).joinpath(run_id,'h5','obj')
+    barcode_filename = raw_dir.joinpath('barcodes.tsv')
+    gene_filename = raw_dir.joinpath('features.tsv')
+    matrix_filename = raw_dir.joinpath('matrix.mtx')
+    h5_filename = h5_dir.joinpath(f'{run_id}.h5ad')
+    ### local temp directories
+    local_spatial_dir = Path(temp_dir).joinpath(spatial_dir)
+    local_raw_dir = Path(temp_dir).joinpath(raw_dir)
+    local_raw_dir.mkdir(parents=True, exist_ok=True)
+    local_h5_dir = Path(temp_dir).joinpath(h5_dir)
+    local_h5_dir.mkdir(parents=True, exist_ok=True)
+
+    local_barcode_filename = local_raw_dir.joinpath('barcodes.tsv')
+    local_gene_filename = local_raw_dir.joinpath('features.tsv')
+    local_matrix_filename = local_raw_dir.joinpath('matrix.mtx')
+    local_h5ad_filename = local_h5_dir.joinpath(f'{run_id}.h5ad')
+    upload_list.append([local_h5ad_filename, h5_filename])
+    upload_list.append([local_barcode_filename, barcode_filename])
+    upload_list.append([local_gene_filename, gene_filename])
+    upload_list.append([local_matrix_filename, matrix_filename])
+
+    f = open(pathBar, 'r')
+    f.close()
+    g = open(pathGene, 'r')
+    g.close()
+    m = open(pathMatrix, 'r')
+    m.close()
+
+    path = Path(root_dir + '/'+ run_id + '/out/Gene/raw')
+    path2 = aws_s3.getFileObject(str(path))
+    adata = sc.read_10x_mtx(str(path2), make_unique="true", var_names="gene_symbols")
+    adata.uns["spatial"] = dict()
+    adata.uns["spatial"]["0"] = dict()
+    
+    files = dict(
+        tissue_positions_file = local_spatial_dir / 'tissue_positions_list.csv',
+        scalefactors_json_file = local_spatial_dir / 'scalefactors_json.json',
+        hires_image = local_spatial_dir / 'tissue_hires_image.png',
+        lowres_image = local_spatial_dir / 'tissue_lowres_image.png'
+    )
+    adata.uns["spatial"]["0"]['images'] = dict()
+    for res in ['hires', 'lowres']:
+        try:
+            adata.uns["spatial"]["0"]['images'][res] = imread(
+                str(files[f'{res}_image'])
+                )
+        except Exception:
+            raise OSError(f"Could not find {res}_image'")
+    self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 40})
+    # read json scalefactors
+    print('good ole chum')
+    adata.uns["spatial"]["0"]['scalefactors'] = json.loads(
+        files['scalefactors_json_file'].read_bytes()
+    )
+    # read coordinates
+    positions = pd.read_csv(files['tissue_positions_file'], header=None)
+    positions.columns = [
+        'barcode',
+        'in_tissue',
+        'array_row',
+        'array_col',
+        'pxl_col_in_fullres',
+        'pxl_row_in_fullres',
+    ]
+        
+    positions.index = positions['barcode']
+
+    adata.obs = adata.obs.join(positions, how="left")
+
+    adata.obsm['spatial'] = adata.obs[
+        ['pxl_row_in_fullres', 'pxl_col_in_fullres']
+    ].to_numpy()
+            
+    adata.obs.drop(
+        columns=['barcode', 'pxl_row_in_fullres', 'pxl_col_in_fullres'],
+        inplace=True,
+    )
+    adata=adata[adata.obs['in_tissue']==1]
+    self.update_state(state="PROGRESS", meta={"position": "Finishing" , "progress" : 80})
+    sc.pp.filter_cells(adata, min_genes=50)
+    sc.pp.filter_genes(adata, min_cells=3)
+    adata.var['mt'] = adata.var_names.str.startswith(('mt-','MT-', 'Mt-'))  # annotate the group of mitochondrial genes as 'mt'
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    adata = adata[adata.obs.pct_counts_mt < 30, :]
+    sc.pp.normalize_total(adata, target_sum= 1e6, exclude_highly_expressed=True)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    sc.pp.neighbors(adata, n_neighbors=10, n_pcs=25)
+    sc.tl.umap(adata, n_components=3)
+    sc.tl.leiden(adata, key_added="clusters")
+    adata.write(local_h5ad_filename)
+    for local_filename, output_key in upload_list:
+        aws_s3.uploadFile(str(local_filename), str(output_key))
+    self.update_state(state="PROGRESS", meta={"position": "Finished" , "progress" : 100})
+    out=[list(map(str, x)) for x in upload_list]
+    return out
+    
